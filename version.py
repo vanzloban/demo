4d0676d5fa09de7f49b3341ca8067f2c5bb32be8
vanzloban <62488618+vanzloban@users.noreply.github.com>
2020-07-08 13:37:30 +0000

    Update version.py

--- version.py
+++ version.py
@@ -1,3 +1,16 @@
 import sys
 
 print(sys.version)
+
+def fizzBuzz():
+  for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+      print('FizzBuzz')
+     elif i % 3 == 0 and not i % 5 == 0:
+      print('Fizz')
+     elif i % 5 == 0 and not i % 3 == 0:
+      print('Buzz')
+     else:
+      print(i)
+      
+fizzBuzz()
